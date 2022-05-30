a75b1349359a2ef89cee7354f4aabb93f292ad71
Tomasz Czerwiński <czerwitom@gmail.com>
2022-05-30 08:08:53 +0200

    Installed basic packages, added tsconfig.json and index.ts file with basic server run.

---
+++ index.ts
@@ -0,0 +1,14 @@
+import express from 'express';
+import cookieParser from 'cookie-parser';
+import morgan from 'morgan';
+
+const app = express();
+
+app.use(express.json());
+app.use(cookieParser());
+app.use(morgan('dev'));
+
+
+app.listen(3001, 'localhost', () => {
+  console.log('Server is listening on http://localhost:3001');
+});
