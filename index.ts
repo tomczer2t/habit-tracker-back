82b9526ece3f7b7835478b9128a3604a5afb3900
Tomasz Czerwiński <czerwitom@gmail.com>
2022-06-13 08:22:54 +0200

    Added routes, cors, credentials and verifyAccessToken middleware to main index file. Changed habitsRouter name.

--- index.ts
+++ index.ts
@@ -1,29 +1,37 @@
 import express, { Router } from 'express';
+import 'express-async-errors';
 import cookieParser from 'cookie-parser';
 import morgan from 'morgan';
 import './utils/db';
-import { habitRouter } from './routes/habit-routes';
-import { corsConfig } from './config/cors.config';
+import { corsConfig } from './config/cors-config';
 import cors from 'cors';
-import 'express-async-errors';
+import { habitsRouter } from './routes/habits-routes';
 import { handleError } from './utils/handleError';
+import { usersRouter } from './routes/users-routes';
+import { sessionsRouter } from './routes/sessions-routes';
+import { credentials } from './middleware/credentials';
+import { verifyAccessToken } from './middleware/verifyAccessToken';
+
 
 const app = express();
 const apiRouter = Router();
 
-app.use(express.json());
+app.use(credentials);
 app.use(cors(corsConfig));
+app.use(express.json());
 app.use(cookieParser());
 app.use(morgan('dev'));
 
 
 app.use('/api', apiRouter);
 
-apiRouter.use('/habits', habitRouter);
+apiRouter.use('/users', usersRouter);
+apiRouter.use('/sessions', sessionsRouter);
+apiRouter.use('/habits', verifyAccessToken, habitsRouter);
 
 app.use(handleError);
 
 
-app.listen(3001, 'localhost', () => {
+app.listen(3001,'0.0.0.0', () => {
   console.log('Server is listening on http://localhost:3001');
 });
--- routes/habits-routes.ts
+++ routes/habits-routes.ts
@@ -1,14 +1,13 @@
-import { Request, Response, Router } from 'express';
+import { Router } from 'express';
 import { HabitController } from '../controllers/habit-controller';
-import { HabitRecord } from '../records/habit-record';
-import { CustomError } from '../utils/handleError';
 
-export const habitRouter = Router();
+export const habitsRouter = Router();
 
-habitRouter.route('/')
+habitsRouter.route('/')
   .get(HabitController.listHabits)
   .post(HabitController.add);
 
-habitRouter.route('/:habitId')
+habitsRouter.route('/:habitId')
+  .get(HabitController.getOne)
   .patch(HabitController.update)
   .delete(HabitController.delete);
